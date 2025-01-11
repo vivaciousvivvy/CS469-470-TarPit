f8f6825a446216a718cb068f94a3bb788550d88f
Jessie Le <phuong2508truc@gmail.com>
2025-01-10 22:17:17 -0800

    modify verifier function

--- main.py
+++ main.py
@@ -99,13 +99,10 @@ config = {"configurable": {"session_id": "Starve_the_Butcher"}}
 
 # Slack command handler
 def slack_handler(request):
-
-    slack_signature = request.headers.get("X-Slack-Signature", "")
-    slack_timestamp = request.headers.get("X-Slack-Request-Timestamp", "")
     request_body = request.get_data()
     verifier = SignatureVerifier(SLACK_SIGNING_SECRET)
 
-    if not verifier.is_valid_request(request_body, slack_signature, slack_timestamp):
+    if not verifier.is_valid_request(request_body, request.headers):
         raise ValueError("Invalid Slack request")
 
     # Parse incoming Slack request
