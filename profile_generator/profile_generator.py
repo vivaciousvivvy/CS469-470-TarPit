846a6dc0f42be90537dda4fe6b575a2b07222787
Riley Woodruff <woodruff.l.riley@gmail.com>
2025-02-13 15:51:55 -0800

    yuh

--- profile_generator/profile_generator.py
+++ profile_generator/profile_generator.py
@@ -25,17 +25,22 @@ image_bytes = query({
 
 image = Image.open(io.BytesIO(image_bytes))"""
 
+#makes the folder if it doesn't exist
+images_folder = "generated_images"
+os.makedirs(images_folder, exist_ok=True)
+
 
 class ProfileGenerator:
 
     def __init__(self):
-        self.PROJECT_ID = "cs470-rag-llm"
+        self.PROJECT_ID = "pigbutchering-capstone"
         vertexai.init(project=self.PROJECT_ID, location="us-west1")
 
     def generate_name(self):
         model = GenerativeModel("gemini-1.5-flash-002")
         name = model.generate_content(f"Generate me a full name, including middle name, for an individual")
         print(name.text)
+        return(name.text)
 
     def generate_bio(self, name):
         model = GenerativeModel("gemini-1.5-flash-002")
@@ -53,6 +58,7 @@ class ProfileGenerator:
                                       The tone must be as if it is were in a biography.")
 
         print(bio.text)
+        return bio.text
 
     def generate_picture(self, bio, name):
 
@@ -78,18 +84,16 @@ class ProfileGenerator:
             #currently person_generation is disabled, I asked for permission to create this from google.
             #person_generation="allow_adult",
         )
-        images[0].save(location=output_file, include_generation_parameters=False)
+        images[0].save(location=f'{images_folder}/{output_file_name}', include_generation_parameters=False)
         print(f"Created output image using {len(images[0]._image_bytes)} bytes")
         # Example response:
         # Created output image using 1234567 bytes
 
 
+        def generate_full(self):
+            pass
 
-def run_main():
-    profile = Profile()
-    profile.generate_name()
-    profile.generate_bio(profile.name)
-    profile.generate_picture("output.png")
 
 if __name__ == "__main__":
-    run_main()
+    generator = ProfileGenerator()
+    generator.generate_name()
